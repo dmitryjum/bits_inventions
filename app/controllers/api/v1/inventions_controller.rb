256c1f5f47897ef2590ab2a03be9313c875a48aa
Dmitry Jum <xfiler@ymail.com>
2018-10-07 17:17:05 -0400

    create action creates new invention

--- app/controllers/api/v1/inventions_controller.rb
+++ app/controllers/api/v1/inventions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::InventionsController < ApplicationController
+  before_action :set_invention, only: :destroy
   def index
     @inventions = Invention.all
     respond_to do |format|
@@ -8,14 +9,54 @@ class Api::V1::InventionsController < ApplicationController
   end
 
   def show
+    #take care of finding by bits or by bit count or by material
+    unless params[:title].nil?
+      @invention = Invention.find_ty_title(params[:title])
+    else
+      @invention = Invention.find(params[:id])
+    end
+    render status: 200, json: @invention
   end
 
   def update
+    #take care if error during database save
+    unless params[:title].nil?
+      @invention = Invention.find_ty_title(params[:title])
+    else
+      @invention = Invention.find(params[:id])
+    end
+    @invention.update(params)
+    render status: 200, json: @invention
   end
 
   def destroy
+    @invention.destroy
+    render status: 204, json: {}
+  end
+
+  def destroy_by_title
+    @invention = Invention.find_by_title(params[:title])
+    @invention.destroy
+    render status: 204, json: {}
   end
 
   def create
+    @invention = Invention.new(invention_params)
+    if @invention.save!
+      render status: 201, json: @invention
+    else
+      render json: @invention.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_invention
+    @invention = Invention.find(params[:id])
+  end
+
+  def invention_params
+    params.require(:invention).permit(:title, :description,
+      :user_name, :user_email, :bits => {}, :materials => [])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
-        resources :inventions
+        resources :inventions do
+          collection do
+            delete :destroy_by_title
+          end
+        end
       end
     end
 end
