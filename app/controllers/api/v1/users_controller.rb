448b49d5465ce4084d71da282f18e324bff58bc5
Dmitry Jum <xfiler@ymail.com>
2019-02-12 23:15:41 -0500

    adds confirm methods to user model and controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,6 +9,19 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def confirm
+    token = params[:token].to_s
+
+    user = User.find_by(confirmation_token: token)
+
+    if user.present? && user.confirmation_token_valid?
+      user.mark_as_confirmed!
+      render json: {status: 'User confirmed successfully'}, status: :ok
+    else
+      render json: {status: 'Invalid token'}, status: :not_found
+    end
+  end
+
   private
 
   def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
-        resources :users, only: :create
+        resources :users, only: :create do
+          collection do
+            post :confirm
+          end
+        end
         resources :inventions do
           collection do
             delete :destroy_by_title
